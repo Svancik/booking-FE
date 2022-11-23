32295b6aa3f7d76bbcc29a8ea11710fe430526cf
Svancik <thesvancik@gmail.com>
2022-11-23 18:59:47 +0100

    navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import "./home.css";
+import Navbar from "./../components/navbar/Navbar";
 
-export default function Home() {
-  return <div>Homeless</div>;
-}
+const Home = () => {
+  return <Navbar />;
+};
+
+export default Home;
