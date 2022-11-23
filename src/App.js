f9fd0c6e321ef7c6f560d28c8ba85f8c1d6a1929
Svancik <thesvancik@gmail.com>
2022-11-23 18:28:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import List from './pages/list/List';
+import Home from './pages/home/Home';
+import Hotel from "./pages/hotel/Hotel";
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    //TODO: Založit nový projekt na GIT booking -FE a tam to programovat
+    <BrowserRouter>  
+    <Routes>
+      <Route path="/" exact element={<Home/>}/>
+      <Route path="/hotels" exact element={<List/>}/>
+      <Route path="/hotels/:id" exact element={<Hotel/>}/>
+    </Routes> 
+    </BrowserRouter>
   );
 }
 
