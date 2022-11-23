5a9a25d57300cfb73694e35141271566b7753262
Svancik <thesvancik@gmail.com>
2022-11-23 19:33:16 +0100

    header

---
+++ src/pages/components/header/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+  faVolleyball,
+} from "@fortawesome/free-solid-svg-icons";
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faVolleyball} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A life of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get reward for your travels - unlock instant savings of 10% or more
+          with a free Lamabooking account.
+        </p>
+        <button className="headerBtn">Sign In / Register</button>
+      </div>
+    </div>
+  );
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./home.css";
 import Navbar from "./../components/navbar/Navbar";
+import { Header } from "./../components/header/Header";
 
-const Home = () => {
-  return <Navbar />;
+export const Home = () => {
+  return (
+    <>
+      <Navbar /> <Header />
+    </>
+  );
 };
 
 export default Home;
