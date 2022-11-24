97c48706df0d7c0d2ea837647e6679551202a839
Svancik <thesvancik@gmail.com>
2022-11-24 21:26:04 +0100

    email section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import { Header } from "./../components/header/Header";
 import { Featured } from "../components/featured/Featured";
 import { PropertyList } from "../components/propertyList/PropertyList";
 import { FeaturedProperties } from "../components/featuredProperties/FeaturedProperties";
+import { MailList } from "../components/mailList/MailList";
 
 export const Home = () => {
   return (
@@ -13,10 +14,11 @@ export const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
-        <h1 className="homeTitle">Browse by property type.</h1>
+        <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
-        <h1 className="homeTitle">Homes guests love.</h1>
+        <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
+        <MailList />
       </div>
     </>
   );
